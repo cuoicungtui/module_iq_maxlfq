d0d0514d0774434db781891833d89320091b7b2e
cuoicungtui <tuongdangvuongquoc901@gmail.com>
2023-11-13 00:09:28 +0700

    fix string get_list

--- src/iqWithPybind11.cpp
+++ src/iqWithPybind11.cpp
@@ -1573,9 +1573,9 @@ SEXP get_list_element(SEXP list, const char *str) {
 //     }
 // }
 
-std::vector<int> get_list_element_int(py::dict list, const char *str) {
+std::vector<std::string> get_list_element_int(py::dict list, const char *str) {
     if (list.contains(str)) {
-        std::vector<int> data = list.attr(str).cast<std::vector<int>>();
+        std::vector<std::string> data = list.attr(str).cast<std::vector<std::string>>();
         return data;
     } else {
         throw std::runtime_error(std::string("Cannot find list element: ") + str);
@@ -1661,7 +1661,7 @@ py::dict iq_MaxLFQ(py::dict list) {
         // quants = list.attr("quant").cast<double>();
 
     }
-    catch (exception & e) {
+    catch (exception & e) {      
         printf("%s\n", e.what());
         return (py::none());
     }
